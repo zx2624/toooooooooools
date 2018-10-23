6886c2d9495fb7c5c2d858f3c318e2c7202252ad
siyuan.yu <1010582011@qq.com>
2018-10-23 15:14:55 +0800

    Update.

--- read_data_from_bag/src/read_bag_odom/script/read_bag_plot_in_google_map.py
+++ read_data_from_bag/src/read_bag_odom/script/read_bag_plot_in_google_map.py
@@ -9,6 +9,8 @@
 from transform_WGS84_GCJ02 import WGS2GCJ
 import rosbag
 import sys
+import getopt
+import glob
 import os
 import time
 from selenium import webdriver
@@ -18,15 +20,10 @@ from gmplot import gmplot
 import subprocess
 
 # read messages 
-def readmsg(topic_name):
-	# open bag
-	bag  = rosbag.Bag(sys.argv[1])
-	path_of_bag = os.path.split(sys.argv[1])[0]
-	print("path of bag: {}".format(path_of_bag))
+def readmsg(topic_name, bags, path_of_bag):
 	# set arrays to store data
 	gps_lat = []
 	gps_lon = []
-
 	# mix lat 
 	min_lat = sys.float_info.max
 	# max lat
@@ -49,47 +46,54 @@ def readmsg(topic_name):
 
 	# set a flag
 	Iter = 0
-	# loop over the topic to read evey message
-	for topic, msg, t in bag.read_messages(topics=topic_name):
-		sec         = t.to_sec() 
-		easting = msg.pose.pose.position.x
-		northing = msg.pose.pose.position.y
-		altitude = msg.pose.pose.position.z
 
-		if max_x < easting:
-			max_x = easting
-		if min_x > easting:
-			min_x = easting
+	for bag_file in bags:
+		# open bag
+		print("Open bag: {}".format(bag_file))
+		bag  = rosbag.Bag(bag_file)
 
-		if max_y < northing:
-			max_y = northing
-		if min_y > northing:
-			min_y = northing
+		# loop over the topic to read evey message
+		for topic, msg, t in bag.read_messages(topics=topic_name):
+			sec         = t.to_sec() 
+			easting = msg.pose.pose.position.x
+			northing = msg.pose.pose.position.y
+			altitude = msg.pose.pose.position.z
 
-		print("easting:{} northing:{} altitude:{}".format(easting, northing, altitude))
-		#convert easting, northing, altitude to latitude, longitude, altitude
-		point = utm.UTMPoint(easting, northing, altitude, 50, 'S').toMsg()
-		print("latitude:{} longitude:{} altitude:{}".format(point.latitude, point.longitude, point.altitude))
-		print("")
+			if max_x < easting:
+				max_x = easting
+			if min_x > easting:
+				min_x = easting
 
-	# transform from GCJ-2 to WGS-84
-		ret = WGS2GCJ(point.latitude, point.longitude)
-		gps_lat.append(ret[0])
-		gps_lon.append(ret[1])
+			if max_y < northing:
+				max_y = northing
+			if min_y > northing:
+				min_y = northing
 
-		if max_lat < ret[0]:
-			max_lat = ret[0]
-		if min_lat > ret[0]:
-			min_lat = ret[0]
+			print("easting:{} northing:{} altitude:{}".format(easting, northing, altitude))
+			#convert easting, northing, altitude to latitude, longitude, altitude
+			point = utm.UTMPoint(easting, northing, altitude, 50, 'S').toMsg()
+			print("latitude:{} longitude:{} altitude:{}".format(point.latitude, point.longitude, point.altitude))
+			print("")
 
-		if max_lon < ret[1]:
-			max_lon = ret[1]
-		if min_lon > ret[1]:
-			min_lon = ret[1]
+		# transform from GCJ-2 to WGS-84
+			ret = WGS2GCJ(point.latitude, point.longitude)
+			gps_lat.append(ret[0])
+			gps_lon.append(ret[1])
 
-		Iter       += 1
+			if max_lat < ret[0]:
+				max_lat = ret[0]
+			if min_lat > ret[0]:
+				min_lat = ret[0]
 
-	bag.close()
+			if max_lon < ret[1]:
+				max_lon = ret[1]
+			if min_lon > ret[1]:
+				min_lon = ret[1]
+
+			Iter += 1
+
+		bag.close()
+
 	print("Read {} points!".format(Iter))
 
 	# get center point's latitude, longitude
@@ -100,15 +104,6 @@ def readmsg(topic_name):
 	# plot the data
 	gmap_plot(gps_lat,gps_lon, path_of_bag, abs(max_x - min_x), abs(max_y - min_y), center_lat, center_lon)
 	screenshot_html(path_of_bag)
-	
-# just read and print the messages on teminal 
-def showmsg(topic_name):
-	bag  = rosbag.Bag(sys.argv[1])
-	Iter = 0
-	for topic, msg, t in bag.read_messages(topics=topic_name):
-		Iter += 1
-		print msg
-	print Iter
 
 # google map plotting
 def gmap_plot(gps_lat, gps_lon, path, x_range, y_range, cen_lat, cen_lon):
@@ -154,8 +149,8 @@ def gmap_plot(gps_lat, gps_lon, path, x_range, y_range, cen_lat, cen_lon):
 	#gmap.plot(gps_lat[1:3], gps_lon[1:3], '#000000', edge_width=10)
 	#gmap.plot(gps_lat[500:], gps_lon[500:], '#ffff00', edge_width=10)
 
-	file_path = path + '/map.html'
-	print('Save map.html to {}'.format(file_path))
+	file_path = path + '/google_map.html'
+	print('Save html to {}'.format(file_path))
 	# Draw
 	gmap.draw(file_path)
 
@@ -170,12 +165,13 @@ def screenshot_html(path):
 	#driver = webdriver.Firefox()
 
 	file_path = 'file://'
-	file_path = file_path +  path + '/map.html'
+	file_path = file_path +  path + '/google_map.html'
 	print("Open html file path:{}".format(file_path))
 	driver.get(file_path)
 	time.sleep(20)
-	driver.save_screenshot(path + '/map.png')
-	print('Save map.png to {}'.format(path + '/map.png'))
+	png_path = path + '/google_map.png'
+	driver.save_screenshot(png_path)
+	print('Save png to {}'.format(png_path))
 	driver.quit()
 
 def get_screen_width_and_height():
@@ -213,14 +209,34 @@ def get_scale(x_range, y_range):
 	# return idx + 1
 	return idx
 
-def main():
+def main(bags, path):
 	# choose one topic  
 	topic_name = '/sensor/novatel/odom'
-	# choose a function
-	readmsg(topic_name)
-	# showmsg(topic_name)
-	# gmap_plot()
+	readmsg(topic_name, bags, path)
+
+def usage():
+	print("1. rosrun test.py -f 0914.bag")
+	print("2. test.py -d /media/0914")
 
 if __name__ == '__main__':
-	print("Read bag: {}".format(sys.argv[1]))
-	main()
+	argv = sys.argv[1:]
+	bags = []
+	path = ''
+	try:
+		opts, args = getopt.getopt(argv, 'hf:d:')
+	except getopt.GetoptError:
+		usage()
+	for opt, arg in opts:
+		if opt == '-h':
+			usage()
+		elif opt == '-f':
+			bags.append(arg)
+			path = os.path.split(arg)[0]
+		elif opt == '-d':
+			bags = glob.glob(arg + '/*.bag')
+			path = arg
+		else:
+			usage()
+		# print('bags: {}'.format(bags))
+		print('path: {}'.format(path))
+		main(bags, path)	
